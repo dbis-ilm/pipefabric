c361374f9cf4c6633121f56cbaaf75b070b7825b
Kai-Uwe Sattler <kus@tu-ilmenau.de>
2016-08-09 08:29:37 +0200

    SeqCEPTest extended (not working yet)

--- src/test/SeqCEPTest.cpp
+++ src/test/SeqCEPTest.cpp
@@ -27,13 +27,7 @@ typedef TuplePtr<InTuple> InTuplePtr;
 typedef Tuple<int, int, int> OutTuple;
 typedef TuplePtr<OutTuple> OutTuplePtr;
 
-/*
-typedef Tuple<int> RelatedTuple;
-typedef TuplePtr<RelatedTuple> RelatedTuplePtr;
-*/
-
 TEST_CASE("Verifying the correct behavior of the CEP operator", "[CEP]") {
-
 	typedef typename RelatedStateValue<InTuplePtr, int, int, 0>::RelatedStateValuePtr RelatedTuplePtr;
 
 	auto mockup = std::make_shared< StreamMockup<InTuplePtr, OutTuplePtr> >("cep_test.in", "cep_test.res");
@@ -69,69 +63,70 @@ TEST_CASE("Verifying the correct behavior of the CEP operator", "[CEP]") {
 	mockup->start();
 }
 
-#if 0
-BOOST_AUTO_TEST_CASE(RelatedSeqCEPTest) {
+#if 1
+TEST_CASE("Verifying the correct behavior of the CEP operator with related values", "[CEP]") {
+	typedef typename RelatedStateValue<InTuplePtr, int, int, 0>::RelatedStateValuePtr RelatedTuplePtr1;
+	typedef typename RelatedStateValue<InTuplePtr, int, int, 0>::RelatedStateValuePtr RelatedTuplePtr2;
+	typedef TuplePtr<Tuple<RelatedTuplePtr1, RelatedTuplePtr2>> RelatedTuplePtr;
 
+	auto mockup = std::make_shared< StreamMockup<InTuplePtr, OutTuplePtr> >("cep_test.in", "cep_test.res");
+	/*
 	auto op1_ = std::make_shared<TupleGenerator<OutTuple>>(
 			"../../src/test/data/cep_test.in");
 	//	qctx.registerOperator("op1", op1_);
 	typedef typename RelatedStateValue<InTuple, int, int, 0>::RelatedStateValuePtr Related0;
 	typedef typename RelatedStateValue<InTuple, int, int, 0>::RelatedStateValuePtr Related1;
 	typedef boost::intrusive_ptr<pfabric::Tuple<Related0, Related1> > RelatedTuple;
-	auto op2_ = new Matcher<InTuple, OutTuple, RelatedTuple>(
-			Matcher<InTuple, OutTuple, RelatedTuple>::FirstMatch);
-	makeLink(op1_, op2_);
-	NFAController<InTuple, OutTuple, RelatedTuple>::NFAControllerPtr nfa =
-			op2_->getNFAController();
-	StartState<InTuple, OutTuple, RelatedTuple>::StartStatePtr stateinit_ =
-			nfa->createStartState("A");
-	NormalState<InTuple, OutTuple, RelatedTuple>::NormalStatePtr stateA =
-			nfa->createNormalState("B");
-	NormalState<InTuple, OutTuple, RelatedTuple>::NormalStatePtr stateB =
-			nfa->createNormalState("C");
-	FinalState<InTuple, OutTuple, RelatedTuple>::FinalStatePtr stateC =
-			nfa->createFinalState("D");
-	ForwardEdge<InTuple, OutTuple, RelatedTuple>::ForwardEdgePtr t4 =
-			nfa->createForwardEdge(
-					[&](const InTuplePtr& tp, const RelatedTuple& rt) {return (std::get<0>(*tp) == 1);});
+	*/
+	auto matcher = new Matcher<InTuplePtr, OutTuplePtr, RelatedTuplePtr>(
+			Matcher<InTuplePtr, OutTuplePtr, RelatedTuplePtr>::FirstMatch);
+	auto nfa = matcher->getNFAController();
+	auto initState = nfa->createStartState("A");
+	auto stateA = nfa->createNormalState("B");
+	auto stateB = nfa->createNormalState("C");
+	auto stateC = nfa->createFinalState("D");
+
+	auto t4 = nfa->createForwardEdge([&](const InTuplePtr& tp, const RelatedTuplePtr& rt) {
+		return (getAttribute<0>(*tp) == 1);
+	});
 	t4->setID(4);
-	nfa->createForwardTransition(stateinit_, t4, stateA);
-	ForwardEdge<InTuple, OutTuple, RelatedTuple>::ForwardEdgePtr t5 =
-			nfa->createForwardEdge(
-					[&](const InTuplePtr& tp, const RelatedTuple& rt) {return (std::get<0>(*tp) == std::get<0>(*rt)->getValue() + 1);});
+	nfa->createForwardTransition(initState, t4, stateA);
+
+	auto t5 = nfa->createForwardEdge([&](const InTuplePtr& tp, const RelatedTuplePtr& rt) {
+		return (getAttribute<0>(*tp) == getAttribute<0>(*rt)->getValue() + 1);
+	});
 	t5->setID(5);
 	nfa->createForwardTransition(stateA, t5, stateB);
-	ForwardEdge<InTuple, OutTuple, RelatedTuple>::ForwardEdgePtr t6 =
-			nfa->createForwardEdge(
-					[&](const InTuplePtr& tp, const RelatedTuple& rt) {return (std::get<0>(*tp) == std::get<1>(*rt)->getValue() + 1);});
+
+	auto t6 = nfa->createForwardEdge([&](const InTuplePtr& tp, const RelatedTuplePtr& rt) {
+		return (getAttribute<0>(*tp) == getAttribute<1>(*rt)->getValue() + 1);
+	});
 	t6->setID(6);
+
 	nfa->createForwardTransition(stateB, t6, stateC);
-	auto init =
-			[&]() {
+	auto init = [&]() {
+		RelatedTuplePtr1 related1 = new RelatedStateValuePrevious<InTuplePtr, int, int, 0 >();
+		RelatedTuplePtr2 related2 = new RelatedStateValuePrevious<InTuplePtr, int, int, 0 >();
+		auto tp = makeTuplePtr(related1, related2);
+		return tp;
+	};
 
-				Related0 related0 = Related0 (new RelatedStateValuePrevious<InTuple, int, int, 0 >() );
-				Related1 related1 = Related1 (new RelatedStateValuePrevious<InTuple, int, int, 0 >() );
-				RelatedTuple tp = makeTuplePtr(related0,related1);
-				return tp;
-			};
-	auto update =
-			[&](const RelatedTuple& tp, int id, const InTuplePtr& event ) {
-				if(id == 4) {
-					auto rel0 = std::get< 0 >(*tp);
-					rel0->updateValue(event);
-				}
-
-				else if(id == 5) {
-					auto rel1 = std::get< 1 >(*tp);
-					rel1->updateValue(event);
-				}
-
-			};
+	auto update = [&](const RelatedTuplePtr& tp, int id, const InTuplePtr& event) {
+		if (id == 4) {
+			auto rel0 = getAttribute<0>(*tp);
+			rel0->updateValue(event);
+		}
+		else if (id == 5) {
+			auto rel1 = getAttribute<1>(*tp);
+			rel1->updateValue(event);
+		}
+	};
 
 	nfa->setDependency(init, update);
 
-	connectChannels(op2_->getOutputChannel<0>(), op1_->getInputChannel<0>());
-	op1_->start();
-	op1_->checkListResult("../../src/test/data/cep_test.res");
+	CREATE_DATA_LINK(mockup, matcher);
+	CREATE_DATA_LINK(matcher, mockup);
+
+	mockup->start();
 }
 #endif
