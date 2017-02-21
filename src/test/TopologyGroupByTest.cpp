9f3f8bf0ec7b089008e44738d86751446529f328
Constantin Pohl <constantin.pohl@tu-ilmenau.de>
2017-02-21 13:30:26 +0100

    Better groupby test

--- src/test/TopologyGroupByTest.cpp
+++ src/test/TopologyGroupByTest.cpp
@@ -188,11 +188,42 @@ TEST_CASE("Building and running a topology with partitioned grouping",
 
   REQUIRE(results.size() == num);
 	
+//sorting (because of unordered results after partitioning)
+  std::vector<std::vector<double>> resultsFirst;
+  std::vector<std::vector<double>> resultsSecond;
+  for(auto i=0u; i<results.size(); i++) {
+	  if(results[i][0]==0) resultsFirst.push_back(results[i]);
+	  else resultsSecond.push_back(results[i]);
+  }
+
+  std::sort(resultsFirst.begin(), resultsFirst.end(), [](const std::vector<double>& a,
+											   const std::vector<double>& b) {return a[1] < b[1]; });
+  std::sort(resultsSecond.begin(), resultsSecond.end(), [](const std::vector<double>& a,
+											   const std::vector<double>& b) {return a[1] < b[1]; });
+
+  std::vector<std::vector<double>> finalResults;
+  for(auto i=0u; i<resultsFirst.size(); i++) {
+	  finalResults.push_back(resultsFirst[i]);
+  }
+  for(auto i=0u; i<resultsSecond.size(); i++) {
+	  finalResults.push_back(resultsSecond[i]);
+  }
+//end of sorting
+
+  //requirement checks
   for (auto i=0u; i<num; i++) {
-	if (results[i][0] == 0 && results[i][1] == 125000) {
-      REQUIRE(results[i][1] == 125000);
-	} else if (results[i][0]>=500) {
-	  REQUIRE(results[i][1] == results[i][0]+0.5);
+    if (i==0) {
+	  REQUIRE(finalResults[i][0] == 0);
+	  REQUIRE(finalResults[i][1] == 0.5);
+	} else if (i<500) {
+	  REQUIRE(finalResults[i][0] == 0);
+	  REQUIRE(finalResults[i][1] == finalResults[i-1][1]+i+0.5);
+	} else if (i==500) {
+	  REQUIRE(finalResults[i][0] == i);
+	  REQUIRE(finalResults[i][1] == 500.5);
+	} else {
+	  REQUIRE(finalResults[i][0] == i);
+	  REQUIRE(finalResults[i][1] == finalResults[i-1][1]+1.0);
 	}
   }
 }
