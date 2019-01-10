2aa84588b45e1f175c2830e73512ad7b1b6d254d
Philipp Götze <philipp.goetze@tu-ilmenau.de>
2019-01-10 13:28:43 +0100

    :copyright: Updated copyright notice

--- src/core/TuplePtrFactory.hpp
+++ src/core/TuplePtrFactory.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2018 DBIS Group - TU Ilmenau, All Rights Reserved.
+ * Copyright (C) 2014-2019 DBIS Group - TU Ilmenau, All Rights Reserved.
  *
  * This file is part of the PipeFabric package.
  *
